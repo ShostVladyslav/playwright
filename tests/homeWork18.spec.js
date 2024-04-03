446acfb9b4997c31227c53c0d0cfd87e6f7a65c0
Vlad <shostvladyslav@gmail.com>
2024-04-03 21:17:17 +0300

    add step test

--- tests/homeWork18.spec.js
+++ tests/homeWork18.spec.js
@@ -25,14 +25,16 @@ test.describe('check SignUP form', ()=>{
             const password = popup.locator('#signupPassword')
             const repeatPassword = popup.locator('#signupRepeatPassword')
             const btnRegister = page.locator('button', {hasText: 'Register'} )
-
-            await fildName.pressSequentially('TestName', {delay: 100})
-            await lastName.pressSequentially('TestLastName', {delay: 100})
-            await email.pressSequentially('aqa-test@email.com', {delay: 100})
-            await password.pressSequentially('Password1', {delay: 100})
-            await repeatPassword.pressSequentially('Password1', {delay: 100})
-            await btnRegister.click()
-            await expect(page).toHaveURL("/panel/garage")
+            
+            await test.step ("User registration", async()=>{
+                await fildName.pressSequentially('TestName', {delay: 100})
+                await lastName.pressSequentially('TestLastName', {delay: 100})
+                await email.pressSequentially('aqa-test@email.com', {delay: 100})
+                await password.pressSequentially('Password1', {delay: 100})
+                await repeatPassword.pressSequentially('Password1', {delay: 100})
+                await btnRegister.click()
+                await expect(page).toHaveURL("/panel/garage")
+            })
         })
     })
 
@@ -42,53 +44,69 @@ test.describe('check SignUP form', ()=>{
             const name = popup.locator('#signupName')
             const nameErrorMessage = popup.locator('#signupName + .invalid-feedback')
             
-            await name.focus()
-            await name.blur()
+            await test.step(`Check the border color and check the "Name required" message`, async()=>{
+                await name.focus()
+                await name.blur()
 
-            await expect(nameErrorMessage).toBeVisible()
-            await expect(nameErrorMessage).toHaveText('Name required')
-            await expect(nameErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+                await expect(nameErrorMessage).toBeVisible()
+                await expect(nameErrorMessage).toHaveText('Name required')
+                await expect(nameErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            })
 
-            await name.fill(' test')
-            await expect(nameErrorMessage).toHaveText('Name is invalid')
+            await test.step('Check message "Name is invalid"', async()=>{
+                await name.fill(' test')
+                await expect(nameErrorMessage).toHaveText('Name is invalid')
+            })
 
-            await name.fill(' ')
-            await expect(nameErrorMessage).toHaveText('Name is invalid'+'Name has to be from 2 to 20 characters long')
-        })
+            await test.step(`Check message "Name is invalid' & 'Name has to be from 2 to 20 characters long'`, async()=>{
+                await name.fill(' ')
+                await expect(nameErrorMessage).toHaveText('Name is invalid'+'Name has to be from 2 to 20 characters long')
+            })  
+    })
 
         test("field Last name", async({page})=>{
             const popup = page.locator('app-signup-modal')
             const lastName = popup.locator('#signupLastName')
             const lastNameErrorMessage = popup.locator('#signupLastName + .invalid-feedback')
+
+            await test.step('Check the border color and check the "Last name required" message', async()=>{
+                await lastName.focus()
+                await lastName.blur()
+
+                await expect(lastNameErrorMessage).toBeVisible()
+                await expect(lastNameErrorMessage).toHaveText('Last name required')
+                await expect(lastNameErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            })
             
-            await lastName.focus()
-            await lastName.blur()
+            await test.step('Check message "Last name is invalid"', async()=>{
+                await lastName.fill(' test')
+                await expect(lastNameErrorMessage).toHaveText('Last name is invalid')
+            })
 
-            await expect(lastNameErrorMessage).toBeVisible()
-            await expect(lastNameErrorMessage).toHaveText('Last name required')
-            await expect(lastNameErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
-
-            await lastName.fill(' test')
-            await expect(lastNameErrorMessage).toHaveText('Last name is invalid')
-
-            await lastName.fill(' ')
-            await expect(lastNameErrorMessage).toHaveText('Last name is invalid'+'Last name has to be from 2 to 20 characters long')
+            await test.step(`Check message "Last name is invalid' & 'Last name has to be from 2 to 20 characters long'`, async()=>{
+                await lastName.fill(' ')
+                await expect(lastNameErrorMessage).toHaveText('Last name is invalid'+'Last name has to be from 2 to 20 characters long')
+            })           
         })
 
         test("field Email", async({page})=>{
             const popup = page.locator('app-signup-modal')
             const email = popup.locator('#signupEmail')
             const emailErrorMessage = popup.locator('#signupEmail + .invalid-feedback')
-            
-            await email.focus()
-            await email.blur()
 
-            await expect(emailErrorMessage).toBeVisible()
-            await expect(emailErrorMessage).toHaveText('Email required')
-            await expect(emailErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            await test.step('Check the border color and check the "Email required" message', async()=>{
+                await email.focus()
+                await email.blur()
 
-            await email.fill(' test')
-            await expect(emailErrorMessage).toHaveText('Email is incorrect')
+                await expect(emailErrorMessage).toBeVisible()
+                await expect(emailErrorMessage).toHaveText('Email required')
+                await expect(emailErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            })
+            await test.step('Check message "Email is incorrec"', async()=>{
+
+                await email.fill(' test')
+                await expect(emailErrorMessage).toHaveText('Email is incorrect')
+            })
 
         })
 
@@ -96,17 +114,21 @@ test.describe('check SignUP form', ()=>{
             const popup = page.locator('app-signup-modal')
             const password = popup.locator('#signupPassword')
             const passwordErrorMessage = popup.locator('#signupPassword + .invalid-feedback')
-            
-            await password.focus()
-            await password.blur()
 
-            await expect(passwordErrorMessage).toBeVisible()
-            await expect(passwordErrorMessage).toHaveText('Password required')
-            await expect(passwordErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            await test.step('Check the border color and check the "Password required" message', async()=>{
+                await password.focus()
+                await password.blur()
 
-            await password.fill('!@# !Password')
-            await expect(passwordErrorMessage).toHaveText('Password has to be from 8 to 15 characters long and \
-                                                            contain at least one integer, one capital, and one small letter')
+                await expect(passwordErrorMessage).toBeVisible()
+                await expect(passwordErrorMessage).toHaveText('Password required')
+                await expect(passwordErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            })
+
+            await test.step('Check validation message for field "Password"', async()=>{
+                await password.fill('!@# !Password')
+                await expect(passwordErrorMessage).toHaveText('Password has to be from 8 to 15 characters long and \
+                                                                contain at least one integer, one capital, and one small letter')
+            })
 
         })
 
@@ -115,22 +137,28 @@ test.describe('check SignUP form', ()=>{
             const password = popup.locator('#signupPassword')
             const repeatPassword = popup.locator('#signupRepeatPassword')
             const repeatPasswordErrorMessage = popup.locator('#signupRepeatPassword + .invalid-feedback')
-            
-            await repeatPassword.focus()
-            await repeatPassword.blur()
 
-            await expect(repeatPasswordErrorMessage).toBeVisible()
-            await expect(repeatPasswordErrorMessage).toHaveText('Re-enter password required')
-            await expect(repeatPasswordErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
-            
-            await repeatPassword.fill('!@# !Password')
-            await expect(repeatPasswordErrorMessage).toHaveText('Password has to be from 8 to 15 characters long and \
-                                                            contain at least one integer, one capital, and one small letter')
+            await test.step('Check the border color and check the "Re-enter password required" message', async()=>{
+                await repeatPassword.focus()
+                await repeatPassword.blur()
 
-            await password.fill('Password1')
-            await repeatPassword.fill('Password123')
+                await expect(repeatPasswordErrorMessage).toBeVisible()
+                await expect(repeatPasswordErrorMessage).toHaveText('Re-enter password required')
+                await expect(repeatPasswordErrorMessage).toHaveCSS('border-color', 'rgb(220, 53, 69)')
+            })
 
-            await expect(repeatPasswordErrorMessage).toHaveText('Passwords do not match')
+            await test.step('Check validation message for field "Re-enter password"', async()=>{
+                await repeatPassword.fill('!@# !Password')
+                await expect(repeatPasswordErrorMessage).toHaveText('Password has to be from 8 to 15 characters long and \
+                                                                contain at least one integer, one capital, and one small letter')
+            })
+
+            await test.step('Check error message "Passwords do not match" for field "Re-enter password"', async()=>{
+                await password.fill('Password1')
+                await repeatPassword.fill('Password123')
+
+                await expect(repeatPasswordErrorMessage).toHaveText('Passwords do not match')
+            })
 
 
         })
@@ -144,19 +172,19 @@ test.describe('check SignUP form', ()=>{
             const password = popup.locator('#signupPassword')
             const repeatPassword = popup.locator('#signupRepeatPassword')
             const btnRegister = page.locator('button', {hasText: 'Register'} )
-
-            await fildName.fill('TestName')
-            await lastName.fill('TestLastName')
-            await email.fill('aqa-test@email.com')
-            await password.fill('Password1')
-            await repeatPassword.fill('Password112')
             
-            await repeatPassword.blur()
+            await test.step('Check disabled button "Register"', async()=>{
+                await fildName.fill('TestName')
+                await lastName.fill('TestLastName')
+                await email.fill('aqa-test@email.com')
+                await password.fill('Password1')
+                await repeatPassword.fill('Password112')
+                
+                await repeatPassword.blur()
 
-            await expect(btnRegister).toBeDisabled()
+                await expect(btnRegister).toBeDisabled()
+            })
         })
 
     })
 })
-
-
