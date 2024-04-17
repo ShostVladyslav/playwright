500c29ea8bc7a9cc402bee47c414595fc80036dd
Vlad <shostvladyslav@gmail.com>
2024-04-17 20:39:30 +0300

    update projects

---
+++ src/pageObjects/BasePage.js
@@ -0,0 +1,20 @@
+import Header from '../pageObjects/GaragePage/Components/Header.js'
+import Main from './GaragePage/Components/Main.js'
+
+
+export default class BasePage {
+    constructor(page, url) {
+        this._page = page
+        this._url = url
+        this.header = new Header(page)
+        this.main = new Main(page)
+    }
+
+    get page (){
+        return this._page
+    }
+
+    async navigate(){
+        await this._page.goto(this._url)
+    }
+}
